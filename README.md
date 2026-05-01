e5fc3b3f4c34d97a6034899e3e06522a199fa45c
Gal Hillel <galh2011@gmail.com>
2026-05-01 11:58:29 +0300

    Final submission

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# reptor-interretialis
+# Reptor-Interretialis
+
+A DevOps class task.
+
+The Python script dynamically generates an Nginx configuration file and a Docker Compose environment. It configures Nginx to route incoming traffic to popular upstream subdomains (test, beta, dev, staging, api) based on a user provided main URL.
+
+
+## How to Run
+
+1. Run, enter the URL and Generate the configuration files:
+
+```bash
+python3 setup.py
+```
+
+2. Build the image and start the container:
+
+```bash
+docker-compose up -d --build
+```
---
+++ setup.py
@@ -0,0 +1,40 @@
+import sys
+
+url = input("Enter main url: ").strip()
+if not url: sys.exit("Error: url cant be empty")
+
+subdomains = ['test', 'beta', 'dev', 'staging', 'api']
+
+subs = "\n".join([f"        server {s}.{url};" for s in subdomains])
+hosts = "\n".join([f"      - '{s}.{url}:127.0.0.1'" for s in subdomains])
+
+conf = f"""events {{
+    worker_connections 1024;
+}}
+http {{
+    upstream backend {{
+{subs}
+    }}
+    server {{
+        listen 80;
+        server_name {url};
+        location / {{ proxy_pass http://backend; }}
+    }}
+}}"""
+
+open("nginx.conf", "w").write(conf)
+print("nginx.conf generated.")
+
+compose = f"""services:
+  web:
+    build: .
+    ports:
+      - "8080:80"
+    volumes:
+      - ./nginx.conf:/etc/nginx/nginx.conf:ro
+    extra_hosts:
+{hosts}
+"""
+
+open("docker-compose.yml", "w").write(compose)
+print("docker-compose.yml generated")
